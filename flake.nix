61026198a228559d8e49045c2074141c16e6037e
rgri <robert13grimley@gmail.com>
2023-06-23 21:02:18 -0500

    Added non-flake input first draft

--- flake.nix
+++ flake.nix
@@ -3,18 +3,17 @@
     # Add to your flake.nix:
     # nwg-look.inputs.nixpkgs.follows = nixpkgs;
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nwg-look-git = {
+      url = "github:nwg-piotr/nwg-look";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs, ... }@inp:
+  outputs = { self, nixpkgs, nwg-look-git, ... }@inp:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in with pkgs; {
       nwg-look = buildGoModule {
         name = "nwg-look";
-        src = fetchFromGitHub {
-          owner = "nwg-piotr";
-          repo = "nwg-look";
-          rev = "c78dfef7f9a98893efdbb978ed7b616c822aeed6";
-          sha256 = "sha256-qA6X1Rr1qEv2H86edNNMpxOggNamc5PdZXmc6cdz9oc=";
-        };
+        src = nwg-look-git;
         nativeBuildInputs = [ pkg-config xcur2png gtk3 ];
         buildInputs = [ cairo glib.dev gtk3.dev ];
         vendorHash = "sha256-5nsXSPjCinzJHtm/YsgJZ6xRlp7P4ArXC0jl1KomHPc=";
