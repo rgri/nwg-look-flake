473abf11f60cba7daa5f064aa1bfd16d763b194e
rgri <robert13grimley@gmail.com>
2023-06-23 20:59:21 -0500

    Working first build
    (I think)

--- flake.nix
+++ flake.nix
@@ -4,23 +4,20 @@
     # nwg-look.inputs.nixpkgs.follows = nixpkgs;
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, ... }@inp: {
-    packages = {
-      default = {
-        x86_64-linux.nwg-look = with nixpkgs;
-          buildGoModule {
-            name = "nwg-look";
-            src = fetchFromGitHub {
-              owner = "nwg-piotr";
-              repo = "nwg-look";
-              rev = "c78dfef7f9a98893efdbb978ed7b616c822aeed6";
-              sha256 = "sha256-qA6X1Rr1qEv2H86edNNMpxOggNamc5PdZXmc6cdz9oc=";
-            };
-            nativeBuildInputs = [ pkg-config xcur2png gtk3 ];
-            buildInputs = [ cairo glib.dev gtk3.dev ];
-            vendorHash = "sha256-5nsXSPjCinzJHtm/YsgJZ6xRlp7P4ArXC0jl1KomHPc=";
-          };
+  outputs = { self, nixpkgs, ... }@inp:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in with pkgs; {
+      nwg-look = buildGoModule {
+        name = "nwg-look";
+        src = fetchFromGitHub {
+          owner = "nwg-piotr";
+          repo = "nwg-look";
+          rev = "c78dfef7f9a98893efdbb978ed7b616c822aeed6";
+          sha256 = "sha256-qA6X1Rr1qEv2H86edNNMpxOggNamc5PdZXmc6cdz9oc=";
+        };
+        nativeBuildInputs = [ pkg-config xcur2png gtk3 ];
+        buildInputs = [ cairo glib.dev gtk3.dev ];
+        vendorHash = "sha256-5nsXSPjCinzJHtm/YsgJZ6xRlp7P4ArXC0jl1KomHPc=";
       };
     };
-  };
 }
